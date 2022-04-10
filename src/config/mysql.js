671648bcd61e13d3ef708a3f712552c467e7f55e
ArinIffana <anaffiadiysor@gmail.com>
2022-04-10 20:04:21 +0700

    just try

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -1,14 +1,14 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  // host: process.env.MYSQL_HOST_ONLINE,
-  // user: process.env.MYSQL_USER_ONLINE,
-  // password: process.env.MYSQL_PASSWORD_ONLINE,
-  // database: process.env.MYSQL_DATABASE_ONLINE,
-  host: process.env.MYSQL_HOST_LOCAL,
-  user: process.env.MYSQL_USER_LOCAL,
-  password: process.env.MYSQL_PASSWORD_LOCAL,
-  database: process.env.MYSQL_DATABASE_LOCAL,
+  host: process.env.MYSQL_HOST_ONLINE,
+  user: process.env.MYSQL_USER_ONLINE,
+  password: process.env.MYSQL_PASSWORD_ONLINE,
+  database: process.env.MYSQL_DATABASE_ONLINE,
+  // host: process.env.MYSQL_HOST_LOCAL,
+  // user: process.env.MYSQL_USER_LOCAL,
+  // password: process.env.MYSQL_PASSWORD_LOCAL,
+  // database: process.env.MYSQL_DATABASE_LOCAL,
 });
 
 connection.connect((error) => {
