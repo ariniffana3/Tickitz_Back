2bf4810a04f589c5a9c0d3ee23b765c9f88d70f6
ArinIffana <anaffiadiysor@gmail.com>
2023-08-28 10:52:13 +0700

    coba

--- src/modules/booking/bookingController.js
+++ src/modules/booking/bookingController.js
@@ -9,6 +9,18 @@ module.exports = {
   createBooking: async (request, response) => {
     try {
       const data = request.body;
+      qr.toFile("public/qr.jpg", "orderId", function (err) {
+        if (err) console.log(err);
+      });
+      const options = {
+        folder: "pesanfilm/imageQr",
+      };
+      var qrCode = "";
+      await cloudinary.uploader
+        .upload("public/qr.jpg", options)
+        .then((result) => {
+          qrCode = result.url;
+        });
       const dataCreate = {
         id: uuidv4(),
         ...data,
